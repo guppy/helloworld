05bb8625b506775577eb5c72e04840d3a13f433d
Jeff Fisher <jeff@zaf.ca>
2026-06-25 05:48:51 -0600

    testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Jeff did a thing!');
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
